428817e278122049c8bee6b04120b7761bc095bd
Mattheus7 <matheusgustavo0008@yaoo.com>
2025-01-14 18:37:20 -0300

    starting project

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const renderUser = function (data) {
+  const avatar = document.querySelector(".user_avatar");
+  const displayName = document.querySelector(".user_name");
+  const userName = document.querySelector(".user_username");
+  const dateJoined = document.querySelector(".user_date-joined");
+  const bio = document.querySelector(".user_bio");
+  const repos = document.querySelector(".user_stats-repo");
+  const followers = document.querySelector(".user_stats-followers");
+  const following = document.querySelector(".user_stats-following");
+  const userLocation = document.querySelector(".user_location");
+  const userWebsite = document.querySelector(".user_info-website");
+  const twitterUrl = document.querySelector(".user_info-twitter");
+  const company = document.querySelector(".user_info-company");
+
+};
